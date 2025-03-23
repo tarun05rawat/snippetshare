b366528eaada713eb9f3557c727e2642afd27a74
Tarun Rawat <144971861+tarun05rawat@users.noreply.github.com>
2025-03-23 01:24:42 -0400

    fixed delete workspace functionality

--- snippetshare-extension/src/utils/api.ts
+++ snippetshare-extension/src/utils/api.ts
@@ -61,6 +61,25 @@ export async function fetchWorkspaces(token: string): Promise<Workspace[]> {
   return response.json() as Promise<Workspace[]>;
 }
 
+//Delete Workspace API
+export async function deleteWorkspace(token: string, workspaceId: string) {
+  const response = await fetch(`${BACKEND_URL}/api/workspaces/${workspaceId}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    const errorResponse = await response.json();
+    const error = errorResponse as { error: string };
+    throw new Error(error.error || "Failed to delete workspace.");
+  }
+
+  return await response.json();
+}
+
 // Fetch snippets API
 export async function fetchSnippets(token: string, workspaceId: string) {
   const response = await fetch(
